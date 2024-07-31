467310a3c6d835f4ff6944b11d67d3cf9ea8d210
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-31 23:50:04 +0530

    contect form  Done

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
     message: "",
   });
   const [responseMessage, setResponseMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const formChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -17,12 +18,46 @@ const Contact = () => {
     });
   };
 
+  async function handlesubmit(e) {
+    e.preventDefault();
+    console.log("run to hua...");
+    try {
+      setFormData((formData.access_key = envConfig.formAccessKey));
+      if (formData.access_key) {
+        const json = JSON.stringify(formData);
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: json,
+        }).then((res) => res.json());
+        if (res.success) {
+          console.log("Success", res);
+          setSuccessMessage(res.message);
+          
+          setTimeout(() => {
+            setSuccessMessage("");
+            setFormData({
+              name: "",
+              mail: "",
+              message: "",
+            });
+            e.target.reset();
+          }, 4000);
+        }
+      }
+    } catch (error) {
+      setResponseMessage(" something went wrong! please try again later... ");
+      console.log("error in handlesubmit :: Contact.jsx :: Components" + error);
+    }
+  }
   return (
     <div className="content w-full min-h-screen pt-24 px-10 flex md:flex-row items-center justify-center flex-col gap-4">
       <div className="leftbox w-full h-full md:w-[30%]  md:h-screen flex-col items-end justify-center gap-0">
         <h1 className="text-slate-900/90 text-3xl font-bold text-center py-2 px-4 my-4">
-          {" "}
-          Talk to me{" "}
+          Talk to me
         </h1>
         <div className="talkcards w-full h-auto flex-wrap flex justify-center px-4 py-2 ">
           <ContactCard
@@ -51,19 +86,26 @@ const Contact = () => {
           Contact me{" "}
         </h1>
         <form
-           action="https://api.web3forms.com/submit" method="POST"
-          className="contenttocontact md:w-[60%] w-full flex-col md:px-6 px-1 py-2 "
+          onSubmit={handlesubmit}
+          className="contenttocontact md:w-[80%] w-full flex-col md:px-8 px-1 py-2 "
         >
-           <input type="hidden" name="access_key" value={envConfig.formAccessKey}/>
+          {/* <input
+            type="hidden"
+            name="access_key"
+            value={envConfig.formAccessKey}
+          /> */}
           <Input
             label={"name "}
             name="name"
+            type="text"
             placeholder="enter your name"
             onChange={formChange}
             parentClassName="my-8 "
           />
+
           <Input
             label={"mail "}
+            type="email"
             name="mail"
             placeholder="enter your mail"
             onChange={formChange}
@@ -71,7 +113,7 @@ const Contact = () => {
           />
           {/* ==================== textarea ============ */}
           <div
-            className={`px-4 py-2 mt-2 rounded-xl w-full flex items-center justify-between border border-gray-700/20 relative my-8`}
+            className={`px-4 py-2 mt-2 rounded-xl w-full flex items-center justify-between border border-gray-700/20 relative mb-2`}
           >
             <label
               htmlFor="message"
@@ -90,8 +132,13 @@ const Contact = () => {
             />
           </div>
           {responseMessage && (
-            <p className="text-red-600 font-semibold bg-white/20 rounded-lg px-4 py-2">
+            <p className="text-red-600 my-2 font-semibold bg-white/20 rounded-lg px-4 py-2">
               {responseMessage}
+            </p>
+          )}
+          {successMessage && (
+            <p className="text-green-500/90 my-2 font-mono text-sm font-bold bg-white/40 rounded-lg px-4 py-2">
+              {successMessage}
             </p>
           )}
           <BigButton
